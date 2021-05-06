3ce5181d114bb2a33f87e575f9a44d57cdb945f8
Pavithra <70309039+Chinreddy-Pavithra@users.noreply.github.com>
2021-05-06 14:37:20 +0530

    Add files via upload

---
+++ loginn.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>User Login</title>
+	<link rel="stylesheet" type="text/css" href="signstyle.css">
+  </head>
+ <body>
+    <div class="header">
+	   <h2>Login</h2>
+	</div>
+<form method="post" action="login.php">
+	<div class="input-group">
+	  <label>Customer Id</label>
+	  <input type="text" name="custid">
+	</div>
+	<div class="input-group">
+	  <label>Password</label>
+	  <input type="password" name="pwd">
+	</div>
+	<div class="input-group">
+	  <button type="submit" name="createaccount" class="btn">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login</button>
+	</div>
+    <p>
+   Not Yet a member?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="signn.html"> Create Account</a>
+	</p>
+</form>
+ </body>
+</html>
